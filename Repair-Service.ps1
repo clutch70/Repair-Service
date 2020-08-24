3c6baaec932f1a1c583417a8016d3d72145edab9
clutch70 <67942178+clutch70@users.noreply.github.com>
2020-08-24 03:19:27 -0500

    Update Repair-Service.ps1

--- Repair-Service.ps1
+++ Repair-Service.ps1
@@ -8,4 +8,58 @@
 #	back with results.													#
 #																		#
 #########################################################################
-	
+
+#Capture the name of the service we want to work on
+$targetService = $args[0]
+
+#Confirm that the service does in fact exist, try the specific service name first
+try {
+		$testSvc = Get-Service -Name $targetService -ErrorAction Stop
+	}
+	#Try the friendly name next
+	catch [System.IO.IOException] {
+			$testSvc = Get-Service -DisplayName $targetService -ErrorAction Stop
+		}
+			#Output NOEXIST and exit if the service passed to us doesn't exist
+			catch{
+				"NOEXIST"
+				exit
+				}
+
+#Now that we've confirmed our variable work with the running status
+IF ($testSvc.status -eq "Running")
+	{
+		try
+			{
+				$svcRestart = Restart-Service -name $targetService -ErrorAction Stop
+			}
+			catch [System.IO.IOException]
+				{
+					$svcRestart = Restart-Service -DisplayName $targetService -ErrorAction Stop
+				}
+				catch
+					{
+						"RESTARTFAIL"
+						exit
+					}
+		write-output "RESTARTED"
+	}
+
+IF ($testSvc.status -eq "Stopped")
+	{
+		try
+			{
+				$svcRestart = Start-Service -name $targetService -ErrorAction Stop
+			}
+			catch [System.IO.IOException]
+				{
+					$svcRestart = Start-Service -DisplayName $targetService -ErrorAction Stop
+				}
+			
+				catch
+					{
+						"CANTSTART"
+						exit
+					}
+		write-output "STARTED"
+	}
