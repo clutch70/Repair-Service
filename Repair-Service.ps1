6fbf2abe68f15d2446b187b50100bf5d9f1dea5e
clutch70 <67942178+clutch70@users.noreply.github.com>
2020-08-24 03:30:58 -0500

    Update Repair-Service.ps1

--- Repair-Service.ps1
+++ Repair-Service.ps1
@@ -5,7 +5,7 @@
 #	Date: 8/21/2020														#
 #	Description: This script determines the status of a given 			#
 #	Windows Service, makes attempts to get it running, and reports 		#
-#	back with simple results suitable for monitoring/ticketing.													#
+#	back with simple results suitable for monitoring/ticketing.			#
 #																		#
 #########################################################################
 
