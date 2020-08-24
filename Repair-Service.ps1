0b6c9676f1e49c2fdf286be11482230d8f0415a3
clutch70 <67942178+clutch70@users.noreply.github.com>
2020-08-24 02:42:54 -0500

    Create Repair-Service.ps1

---
+++ Repair-Service.ps1
@@ -0,0 +1,11 @@
+#########################################################################
+#																		#
+#	Script Title: Repair-Service										#
+#	Author: Brennan Custard												#
+#	Date: 8/21/2020														#
+#	Description: This script determines the status of a given 			#
+#	Windows Service, makes attempts to get it running, and reports 		#
+#	back with results.													#
+#																		#
+#########################################################################
+	
